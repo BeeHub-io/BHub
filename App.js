6c5521992415e85ec0c6ea76d001fb7c67b7c0e5
tysong24 <tommysong123@gmail.com>
2020-10-27 15:46:47 -0700

    working get current position. need to fix map relocation after getting current position

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
 import React from 'react';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
+// import Geolocation from '@react-native-community/geolocation';
 
 export default class App extends React.Component {
+  // NEED STATE TO HOLD STUFF
+  constructor(props) {
+    super(props);
+    this.state = {
+      position: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.05,
+        longitudeDelta: 0.05,
+      },
+    };
+  }
+
+  // BUILD A ONCLICK FUNCTION FOR WHEN THE MARKER IS CLICKED
+
+  // SHOULD HAPPEN WITH COMPONENT DID MOUNT
+  componentDidMount() {
+    // A FUNCTION THAT SENDS LONGITUDE AND LATITUDE TO BACKEND THAT RECEIVES LIST OF RESTAURANTS THAT MAPS IT INTO MARKER JSX FORMAT INTO AN ARRAY
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log('current position: ', position);
+      console.log('latitude: ', position['coords']['latitude']);
+      let newPosition = {
+        latitude: position['coords']['latitude'],
+        longitude: position['coords']['longitude'],
+        latitudeDelta: 0.05,
+        longitudeDelta: 0.05,
+      };
+      this.setState({ position: newPosition });
+    });
+  }
+
+  componentDidUpdate() {
+    console.log('YOOO THIS SOME HOOPLAH: ', this.state);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <MapView
-          style={styles.mapStyle}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        />
+        <MapView style={styles.mapStyle} initialRegion={this.state.position}>
+          {/* DISPLAY MARKERS ARRAY */}
+          {/* <Marker
+            coordinate={{
+              latitude: 37.78825329977967,
+              longitude: -122.4324329977967,
+            }}
+            pinColor='blue'
+          /> */}
+        </MapView>
       </View>
     );
   }
