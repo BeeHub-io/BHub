e14446c62a02adaf23d6a77ee00445079319a09b
davidemmolino <davidewebdev@gmail.com>
2020-10-27 15:59:26 -0700

    fetch for yelp api

--- server/controller.js/ApiController.js
+++ server/controller.js/ApiController.js
@@ -1,4 +1,17 @@
 const db = require('../models/TaskModel');
 const controller = {};
 
+// Making request to yelp api to retrieve restaurants
+const getAllRestaurants = async () => {
+    // static coordiantes, will change 
+    const queryString = '?latitude=37.78825&longitude=-122.4324';
+    try {
+        await fetch(`https://api.yelp.com/v3/businesses/search/${queryString}/`)
+              .then(response => response.json())
+              .then(data => console.log(data));
+    } catch(err) {
+        return next(err);
+    };
+};
+
 module.exports = controller;
--- server/routes.js
+++ server/routes.js
@@ -2,4 +2,9 @@ const express = require('express');
 const taskController = require('./controller/ApiController.js');
 const router = express.Router();
 
+// post request to get restaurants based off of user coordinates
+router.get('/', controller.getAllRestaurants,(req, res) => {
+    res.status(200).json(res.locals.allRestaurants);
+})
+
 module.exports = router;
