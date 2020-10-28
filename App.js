e235143d7cae81e9530f6e89c6e3e233eda18d35
tysong24 <tommysong123@gmail.com>
2020-10-28 14:28:21 -0700

    moved fetch request to frontend

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ export default class App extends React.Component {
         latitudeDelta: 0.05,
         longitudeDelta: 0.05,
       },
+      radius: 8047,
       search: '',
     };
   }
@@ -36,16 +37,7 @@ export default class App extends React.Component {
       };
       this.setState({ position: newPosition });
     });
-    // axios
-    //   .post('192.168.1.127:19000/api', {
-    //     latitude: this.state.position.latitude,
-    //     longitude: this.state.position.longitude,
-    //   })
-    //   .then(() => console.log('Position was sent to server!'))
-    //   .catch((err) =>
-    //     console.log('ERROR IN COMPONENT DID MOUNT > AXIOS > POST ERROR: ', err)
-    //   );
-    fetch('https://localhost:3333/api/getAll', {
+    fetch('http://192.168.1.127:3333/api/getAll', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -62,6 +54,32 @@ export default class App extends React.Component {
 
   componentDidUpdate() {
     console.log('YOOO THIS SOME HOOPLAH: ', this.state);
+    let latitude = this.state.position.latitude;
+    let longitude = this.state.position.longitude;
+    let radius = this.state.radius;
+    const queryAllString = `?latitude=${latitude}&longitude=${longitude}&radius=${radius}`;
+    fetch(`https://api.yelp.com/v3/businesses/search/${queryAllString}/`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization:
+          'BHgF17sVB1nq84WpSrlu-w8fiCq3VIwXsImhNA1AZjcPt7UFCPuebY8nhglwQV5PYdnLCkUbM8Gw23SYrL0scYR_T9K6w_unJVqZ5H2wRgt29ORpu9X8F-5WKaKYX3Yx',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // filter out out chains and franchises, with more than 50 reviews ??
+        // OR if we want to specify restaurants we need to do it manually
+        // e.g array of restaurants to filter out
+        // const notFranchises = data.filter((el) => el.review_count < 50);
+        console.log('data: ', data);
+      })
+      .catch((err) =>
+        console.log(
+          'ERROR IN COMPONENT DID MOUNT > FETCH > YELP API ERROR: ',
+          err
+        )
+      );
   }
 
   updateSearch = (search) => {
@@ -77,7 +95,7 @@ export default class App extends React.Component {
           lightTheme
           searchIcon={{ size: 36 }}
           style={styles.searchBar}
-          placeholder=''
+          placeholder='Search'
           onChangeText={this.updateSearch}
           value={this.state.search}
         />
